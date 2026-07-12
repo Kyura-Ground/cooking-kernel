51f29fe7d0d2d12f9fce1c9db76e2f07b0ba7285
ikwfahmi <mikhwanfahmi4@gmail.com>
2026-07-12 23:12:07 +0700

    build: Use gcc-master branch for GCC toolchains
    Signed-off-by: ikwfahmi <mikhwanfahmi4@gmail.com>

--- build.sh
+++ build.sh
@@ -44,9 +44,9 @@ CLANG_URL="${CLANG_URL:-https://android.googlesource.com/platform/prebuilts/clan
 
 USE_GCC_CROSS="${USE_GCC_CROSS:-1}" # Set to 1 to use Clang x GCC 11.2.1
 GCC_64_REPO="${GCC_64_REPO:-https://github.com/mvaisakh/gcc-arm64}"
-GCC_64_BRANCH="${GCC_64_BRANCH:-gcc-new}"
+GCC_64_BRANCH="${GCC_64_BRANCH:-gcc-master}"
 GCC_32_REPO="${GCC_32_REPO:-https://github.com/mvaisakh/gcc-arm}"
-GCC_32_BRANCH="${GCC_32_BRANCH:-gcc-new}"
+GCC_32_BRANCH="${GCC_32_BRANCH:-gcc-master}"
 
 # Build Options
 BUILD_KSU="${BUILD_KSU:-1}" # Set to 1 to enable KernelSU, 0 to disable
